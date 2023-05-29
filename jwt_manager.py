f902bdc663dc30b109e8cecb560d91810ad3330c
Daniel Gonzalez <danielgonzalez20020110@gmail.com>
2023-05-29 17:54:33 -0400

    Jwt tokens

---
+++ jwt_manager.py
@@ -0,0 +1,14 @@
+from jwt import encode, decode
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+secret_key = os.getenv('SECRET_KEY')
+
+def create_token(data: dict) -> str:
+    token: str = encode(payload=data, key=secret_key, algorithm="HS256")
+    return token
+
+def validate_token(token: str) -> dict:
+    data: dict = decode(token, key=secret_key, algorithms=['HS256'])
+    return data
