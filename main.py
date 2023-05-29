2c5bfb8d311b8831b83e85f83cf5ecea1e1d32e1
Daniel Gonzalez <danielgonzalez20020110@gmail.com>
2023-05-29 12:23:40 -0400

    Metodos get, post, put y delete

---
+++ main.py
@@ -0,0 +1,91 @@
+from fastapi import FastAPI, HTTPException, Body
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+
+app = FastAPI()
+app.title = "Mi aplicación con  FastAPI"
+app.version = "0.0.1"
+
+movies = [
+    {
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	},
+    {
+		"id": 2,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	}
+]
+
+@app.get('/', tags=['home'])
+def message():
+    return HTMLResponse('<h1>Hello world</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
+
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie(id: int):
+    for item in movies:
+        if item["id"] == id:
+            return item
+    raise HTTPException(status_code=404, detail="Movie not found")
+
+@app.get('/movies/', tags=['movies'])
+def get_movies_by_category(category: str, year: int):
+    # List Comprenhension
+    return [movie for movie in movies if movie['category'] == category]
+
+@app.post('/movies', tags=['movies'])
+def create_movie(id: int = Body(), title: str = Body(), overview:str = Body(), year:int = Body(), rating: float = Body(), category: str = Body()):
+    movies.append({
+        "id": id,
+        "title": title,
+        "overview": overview,
+        "year": year,
+        "rating": rating,
+        "category": category
+    })
+    return movies
+# otra forma de hacer el post
+    # async def create_movie(request: Request):
+    # movie = await request.json()
+
+    # movies.append(movie)
+
+    # return movie
+
+@app.delete('/movies', tags=['movies'])
+def delete_movie(id: int):
+    for item in movies:
+        if(item['id'] == id):
+            movies.remove(item)
+            return movies
+             
+            
+
+@app.put('/movies/{id}', tags=['movies'])
+def update_movie(id: int, title: str = Body(), overview:str = Body(), year:int = Body(), rating: float = Body(), category: str = Body()):
+    for item in movies:
+	    if item["id"] == id:
+                item['title'] = title
+                item['overview'] = overview
+                item['year'] = year
+                item['rating'] = rating
+                item['category'] = category
+                return movies
+                    
+                    
+		    
+
+    
+
