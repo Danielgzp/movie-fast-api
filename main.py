72fdfec37ca05676272e4a90f87d08c7b9bb92f9
Daniel Gonzalez <danielgonzalez20020110@gmail.com>
2023-05-31 18:33:19 -0400

    Modularizacion y refactorizaciones finales

--- main.py
+++ main.py
@@ -1,51 +1,27 @@
-from fastapi import FastAPI, HTTPException, Body, Path, Query, Request, Depends
-from fastapi.responses import HTMLResponse, JSONResponse
-from pydantic import BaseModel, Field
-from typing import Optional
-from jwt_manager import create_token, validate_token
-from fastapi.security import HTTPBearer
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from config.database import engine, Base
+from middlewares.error_handler import ErrorHandler
+from routers.movies import movie_router
+from routers.user import user_router
 
 app = FastAPI()
 app.title = "Mi aplicación con  FastAPI"
 app.version = "0.0.1"
+app.add_middleware(ErrorHandler)
+app.include_router(user_router)
+app.include_router(movie_router)
 
-class JWTBearer(HTTPBearer):
-    async def __call__(self, request: Request):
-        auth = await super().__call__(request)
-        data = validate_token(auth.credentials)
-        if data['email'] != "admin@gmail.com":
-            raise HTTPException(status_code=403, detail="Credenciales son invalidas")
 
-class User(BaseModel):
-    email:str
-    password:str
-#Este vendria siendo el Schema
-class Movie(BaseModel):
-    id: Optional[int] = None
-    title: str = Field(min_length=5, max_length=15)
-    overview: str = Field(min_length=15, max_length=50)
-    year: int = Field(le=2022)
-    rating:float = Field(ge=1, le=10)
-    category:str = Field(min_length=3, max_length=15)
-    '''
-    gt: greater than
-    ge: greater than or equal
-    lt: less than
-    le: less than or equal
+Base.metadata.create_all(bind=engine)
 
-    '''
+# class JWTBearer(HTTPBearer):
+#     async def __call__(self, request: Request):
+#         auth = await super().__call__(request)
+#         data = validate_token(auth.credentials)
+#         if data['email'] != "admin@gmail.com":
+#             raise HTTPException(status_code=403, detail="Credenciales son invalidas")
 
-    class Config:
-        schema_extra = {
-            "example": {
-                "id": 1,
-                "title": "Mi película",
-                "overview": "Descripción de la película",
-                "year": 2022,
-                "rating": 0,
-                "category" : "Nombre de la categoria"
-            }
-        }
 
 movies = [
     {
@@ -70,65 +46,7 @@ movies = [
 def message():
     return HTMLResponse('<h1>Hello world</h1>')
 
-@app.post("/login", tags=["auth"])
-def login(user: User):
-    if (user.email == "admin@gmail.com" and user.password == "123456"):
-        token: str = create_token(user.dict())
-        return JSONResponse(status_code=200, content=token)
-    else:
-        return JSONResponse(status_code=401, content={"message": "Credenciales inválidas, intente de nuevo"})
 
-@app.get('/movies', tags=['movies'], dependencies=[Depends(JWTBearer())])
-def get_movies():
-    return movies
-
-@app.get('/movies/{id}', tags=['movies'])
-def get_movie(id: int = Path(ge=1, le=2000)):
-    for item in movies:
-        if item["id"] == id:
-            return item
-    raise HTTPException(status_code=404, detail="Movie not found")
-#   return JSONResponse(status_code=404, content=[])
-
-@app.get('/movies/', tags=['movies'])
-def get_movies_by_category(category: str = Query(min_length=5, max_length=15)):
-    # List Comprenhension
-    data = [movie for movie in movies if movie['category'] == category]
-    return data
-
-@app.post('/movies', tags=['movies'], response_model=dict, status_code=201)
-def create_movie(movie: Movie) -> dict:
-    movies.append(movie)
-    # movie.dict()
-    return JSONResponse(status_code=201, content={"message": "Se ha registrado correctamente la película"})
-# otra forma de hacer el post
-    # async def create_movie(request: Request):
-    # movie = await request.json()
-
-    # movies.append(movie)
-
-    # return movie
-
-@app.delete('/movies', tags=['movies'], response_model=dict, status_code=200)
-def delete_movie(id: int = Path(ge=1, le=2000)) -> dict:
-    for item in movies:
-        if(item['id'] == id):
-            movies.remove(item)
-            return JSONResponse(status_code=200, content={"message": "Se ha eliminado correctamente la película"})
-             
-            
-#response_model=dict con esto estamos diciendo de que nos devuelva un diccionario
-@app.put('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
-#-> dict | con esto estamos diciendo de que nos devuelva un diccionario
-def update_movie(id: int, movie: Movie) -> dict:
-    for item in movies:
-	    if item["id"] == id:
-                item['title'] = movie.title
-                item['overview'] = movie.overview
-                item['year'] = movie.year
-                item['rating'] = movie.rating
-                item['category'] = movie.category
-                return JSONResponse(status_code=200, content={"message": "Se actualizo correctamente la película"})
                     
                     
 		    
---
+++ routers/movies.py
@@ -0,0 +1,58 @@
+from fastapi import  Path, Query, Depends, APIRouter
+from fastapi.responses import JSONResponse
+from config.database import Session
+from models.movie import Movie as MovieModel
+from fastapi.encoders import jsonable_encoder
+from middlewares.jwt_bearer import JWTBearer
+from services.movie import MovieService
+from schemas.movie import Movie
+
+movie_router = APIRouter()
+
+@movie_router.get('/movies', tags=['movies'], dependencies=[Depends(JWTBearer())])
+def get_movies():
+    db = Session()
+    result = MovieService(db).get_movies()
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
+
+@movie_router.get('/movies/{id}', tags=['movies'])
+def get_movie(id: int = Path(ge=1, le=2000)):
+    db = Session()
+    result = MovieService(db).get_movie(id)
+    if not result:
+        return JSONResponse(status_code=404, content={'message': "No encontrado"})
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
+#   return JSONResponse(status_code=404, content=[])
+
+@movie_router.get('/movies/', tags=['movies'])
+def get_movies_by_category(category: str = Query(min_length=5, max_length=15)):
+    db = Session()
+    result = MovieService(db).get_movies_by_category(category)
+    if not result:
+        return JSONResponse(status_code=404, content={'message': "Categoria no encontrada"})
+    return JSONResponse(status_code=200, content=jsonable_encoder(result))
+
+@movie_router.post('/movies', tags=['movies'], response_model=dict, status_code=201)
+def create_movie(movie: Movie) -> dict:
+    db = Session()
+    MovieService(db).create_movie(movie)
+    return JSONResponse(status_code=201, content={"message": "Se ha registrado correctamente la película"})
+
+@movie_router.delete('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
+def delete_movie(id: int)-> dict:
+    db = Session()
+    result = MovieService(db).delete_movie(id)
+    return result
+
+#response_model=dict con esto estamos diciendo de que nos devuelva un diccionario
+@movie_router.put('/movies/{id}', tags=['movies'], response_model=dict, status_code=200)
+#-> dict | con esto estamos diciendo de que nos devuelva un diccionario
+def update_movie(id: int, movie: Movie) -> dict:
+    db = Session()
+    result = MovieService(db).get_movie(id)
+    if not result:
+        return JSONResponse(status_code=404, content={'message': "No encontrado"})
+    else: 
+        MovieService(db).update_movie(id, movie)
+        return JSONResponse(status_code=200, content={"message": "Se ha modificado la película"})
+                    
