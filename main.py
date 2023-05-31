6e3bd8e00deaf998dd74b9aacc496e6e58bd8cca
Daniel Gonzalez <danielgonzalez20020110@gmail.com>
2023-05-31 18:57:23 -0400

    Correciones para hacer el deploy

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from config.database import engine, Base
@@ -5,13 +7,13 @@ from middlewares.error_handler import ErrorHandler
 from routers.movies import movie_router
 from routers.user import user_router
 
+
 app = FastAPI()
 app.title = "Mi aplicación con  FastAPI"
 app.version = "0.0.1"
 app.add_middleware(ErrorHandler)
 app.include_router(user_router)
 app.include_router(movie_router)
-
 
 Base.metadata.create_all(bind=engine)
 
@@ -45,6 +47,12 @@ movies = [
 @app.get('/', tags=['home'])
 def message():
     return HTMLResponse('<h1>Hello world</h1>')
+
+#Esto es para hacerle deploy a la aplicacion en Railway
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0",
+                port=int(os.environ.get("PORT", 8000)))
+
 
 
                     
--- middlewares/error_handler.py
+++ middlewares/error_handler.py
@@ -1,12 +1,15 @@
 from starlette.middleware.base import BaseHTTPMiddleware
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse
+from typing import Union
 
 class ErrorHandler(BaseHTTPMiddleware):
     def __init__(self, app: FastAPI) -> None:
         super().__init__(app)
 
-    async def dispatch(self, request: Request, call_next) -> Response | JSONResponse:
+    # Asi es como estaba originalmente, pero por razones de deploy se tuvo que modificar esta linea por las versiones de python
+    # async def dispatch(self, request: Request, call_next) -> Response | JSONResponse:
+    async def dispatch(self, request: Request, call_next) -> Union[Response, JSONResponse]:
         try:
             return await call_next(request)
         except Exception as e:
